04c99b910df49ae363d3b507875024e0be7d93f3
Jishan Ahamed <jjahamed33@gmail.com>
2025-02-10 23:18:39 +0530

    Initial Setup

---
+++ 01_springcore/src/main/java/com/jishan/practice/springcore/App.java
@@ -0,0 +1,13 @@
+package com.jishan.practice.springcore;
+
+/**
+ * Hello world!
+ *
+ */
+public class App 
+{
+    public static void main( String[] args )
+    {
+        System.out.println( "Hello World!" );
+    }
+}
