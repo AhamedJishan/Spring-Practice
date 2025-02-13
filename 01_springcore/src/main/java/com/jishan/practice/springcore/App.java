76403ac3757a3571e7118ce4cf989dee10202e0b
Jishan Ahamed <jjahamed33@gmail.com>
2025-02-13 19:19:39 +0530

    01: Bean with p schema setter injection

--- 01_springcore/src/main/java/com/jishan/practice/springcore/App.java
+++ 01_springcore/src/main/java/com/jishan/practice/springcore/App.java
@@ -1,5 +1,8 @@
 package com.jishan.practice.springcore;
 
+import org.springframework.context.ApplicationContext;
+import org.springframework.context.support.ClassPathXmlApplicationContext;
+
 /**
  * Hello world!
  *
@@ -8,6 +11,16 @@ public class App
 {
     public static void main( String[] args )
     {
-        System.out.println( "Hello World!" );
+    	
+    	ApplicationContext appContext = new ClassPathXmlApplicationContext("config.xml");
+    	
+    	Student student1 = (Student) appContext.getBean("student1");
+    	System.out.println(student1);
+    	
+    	Student student2 = (Student) appContext.getBean("student2");
+    	System.out.println(student2);
+    	
+    	Student student3 = (Student) appContext.getBean("student3");
+    	System.out.println(student3);
     }
 }
